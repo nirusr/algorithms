eafb4e3f66f656cf81042c9ea87a874c389bfed0
Srinivasan Govindaraji <sgovindaraji@walmartlabs.com>
2016-08-20 08:32:55 -0700

    Added count of nodes at levels

--- src/main/java/com/ani/algorithms/BinarySearchTree.java
+++ src/main/java/com/ani/algorithms/BinarySearchTree.java
@@ -4,6 +4,7 @@ package com.ani.algorithms;
 public class BinarySearchTree {
 	
 	public static Node root;
+	public static int count = 1;
 	public BinarySearchTree() {
 		// TODO Auto-generated constructor stub
 		this.root = null;
@@ -51,6 +52,8 @@ public class BinarySearchTree {
 				}
 			}
 			
+			if ( id == current.data) return; //duplicate insert
+			
 			
 			
 		}
@@ -67,22 +70,223 @@ public class BinarySearchTree {
 		}
 	}
 	
+	public static void printBinaryTree(Node root, int level){
+	    if(root==null)
+	         return;
+//	    System.out.println("pBT(" + root.right.data + "," + level + 1);
+	    printBinaryTree(root.right, level+1);
+	   
+	    if(level!=0){
+	        for(int i=0;i<level-1;i++)
+	            System.out.print("|\t");
+	            System.out.println("|-------"+root.data);
+	            count++;
+	    }
+	    else {
+	        System.out.println(root.data);
+	        count++;
+	    }
+//	    System.out.println("pBT(" + root.left.data + "," + level + 1);
+	    printBinaryTree(root.left, level+1);
+	   
+	}    
 	
 	
+	
+	public int printNodeValueL(Node node, int level) {
+		int count = 0;
+		if (node == null) {
+			return 0;
+		} else {
+			//System.out.print(node.data);
+			//System.out.println("," + level);
+			if ( level == 1) {
+				System.out.println(level + "," + node.data );
+				
+
+					if ( node.left != null ) {
+						System.out.println( "Left:" + node.left.data);
+						count ++;
+					}
+					if ( node.right != null) {
+						System.out.println("Right:" + node.right.data);
+						count ++;
+						
+					}
+				System.out.println("count:" + count);
+				return count;
+				
+			}
+			
+		}
+		return count + printNodeValueL(node.left, level + 1);
+		//count + printNodeValue(node.right, level + 1);
+		
+		//System.out.println(node.data);
+		
+		
+		
+
+	}
+	
+	public int printNodeValueR(Node node, int level) {
+		int count = 0;
+		if (node == null) {
+			return 0;
+		} else {
+			//System.out.print(node.data);
+			//System.out.println("," + level);
+			if ( level == 1) {
+				System.out.println(level + "," + node.data );
+				
+
+					if ( node.left != null ) {
+						System.out.println( "Left:" + node.left.data);
+						count ++;
+					}
+					if ( node.right != null) {
+						System.out.println("Right:" + node.right.data);
+						count ++;
+						
+					}
+				System.out.println("count:" + count);
+				return count;
+				
+			}
+			
+		}
+		//return count + printNodeValue(node.left, level + 1);
+		return count + printNodeValueR(node.right, level + 1);
+		
+		//System.out.println(node.data);
+		
+		
+		
+
+	}
+	
+	public int printNodeCount(Node node, int level) {
+		return printNodeValueL(node, level) + printNodeValueR(node, level);
+	}
+	
+	public boolean find(int id) {
+		Node current = root;
+		while( current != null) {
+			
+			if ( id < current.data) {
+				current = current.left;
+				if ( current == null ) return false;
+			}
+			
+			if (id > current.data) {
+				current = current.right;
+				if ( current == null) return false;
+			}
+			
+			if ( id == current.data) {
+				return true;
+			}
+			
+		}
+		
+		return false;
+	}
+	
+	
+	//find # of nodes To Be Worked.
+	public int getLeafCount() {
+		//int count = 1;
+		
+		return getLeafCount(root);
+		
+		
+	}
+	
+	public int getLeafCount(Node node) {
+		if ( node == null) {
+			return 0;
+		} 
+		if ( node.left == null && node.right == null) {
+			return 1;
+		} else {
+			return getLeafCount(node.left) + getLeafCount(node.right);
+		}
+			
+	}
+	
+	public int countNodeAtLevel(Node node, int level) {
+		int count=0;
+		if ( node == null) return 0;
+		if ( level == 0) {
+			return 0;
+		} else {
+			if ( node.left != null ) {
+				count++;
+				return count + countNodeAtLevel(node.left, level + 1);
+			} else {
+				return 0;
+			}
+			
+		}
+		
+	}
 	
 	//TEST
 	public static void main(String[] args) {
 		BinarySearchTree bst = new BinarySearchTree();
-		bst.insert(20);
+		/*bst.insert(25);
+		bst.insert(16);
+		bst.insert(15);
 		bst.insert(10);
-		bst.insert(1);
-		bst.insert(2);
-		bst.insert(3);
-		bst.insert(4);
-		bst.insert(6);
+		bst.insert(9);
 		bst.insert(8);
-		bst.insert(21);
+		bst.insert(6);
+		bst.insert(4);
+		bst.insert(3);
+		bst.insert(2);
+		bst.insert(1);
+		bst.insert(12);
+		bst.insert(11);
+		bst.insert(14);
+		bst.insert(15);*/
+		bst.insert(50);
+		bst.insert(40);
+		bst.insert(45);
+		bst.insert(46);
+		bst.insert(44);
+		bst.insert(30);
+		
+		
+		bst.insert(80);
+		bst.insert(20);
+		bst.insert(35);
+		bst.insert(100);
+		bst.insert(90);
+		bst.insert(110);
+		
+		
+		/*
+		 *              20
+		 *            10
+		 *          1
+		 *             2
+		 *          
+		 *          
+		 *          
+		 */
+		
 		bst.display(bst.root);
+		System.out.println("\n");
+		bst.printBinaryTree(bst.root, 0);
+	
+		System.out.println("\n" + bst.find(22));
+		//System.out.println("\n" + bst.getLeafCount() );
+		System.out.println(count);
+		bst.printNodeValueL(root, 0) ;
+		bst.printNodeValueR(root, 0) ;
+		System.out.println(bst.printNodeCount(root, 0));
+		
+		System.out.println("=" + bst.countNodeAtLevel(root,1));
 	}
 	
 
